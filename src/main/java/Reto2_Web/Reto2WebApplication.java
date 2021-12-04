78456491eaf080ea04b1989bd7a00757b116f6d8
Mateo <mpachon32@gmail.com>
2021-12-04 12:19:13 -0500

    second commit

--- src/main/java/Reto2_Web/Reto2WebApplication.java
+++ src/main/java/Reto2_Web/Reto2WebApplication.java
@@ -12,7 +12,9 @@ import Reto2_Web.interfaces.InterfaceUser;
 	import org.springframework.boot.CommandLineRunner;
 	import org.springframework.boot.SpringApplication;
 	import org.springframework.boot.autoconfigure.SpringBootApplication;
+        import org.springframework.stereotype.Component;
 	
+        @Component
 	@SpringBootApplication
 	public class Reto2WebApplication implements CommandLineRunner {
 	   @Autowired
